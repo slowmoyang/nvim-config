dd51cb8d7f84884e1a02eb032b10dd0c66adb11b
Seungjin Yang <slowmoyang@gmail.com>
2023-04-17 14:20:28 +0900

    enable keymap for lua-snip and add some snippets

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -33,6 +33,23 @@ lsp.nvim_workspace()
 
 lsp.setup()
 
+--
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-j>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+  }
+})
 
 -- https://neovim.io/doc/user/diagnostic.html
 vim.diagnostic.config({
--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,18 +1,1 @@
 require("luasnip.loaders.from_vscode").lazy_load({paths = "./snippets"})
-
--- press <Tab> to expand or jump in a snippet. These can also be mapped separately
--- via <Plug>luasnip-expand-snippet and <Plug>luasnip-jump-next.
-vim.cmd[[imap <silent><expr> <Tab> luasnip#expand_or_jumpable() ? '<Plug>luasnip-expand-or-jump' : '<Tab>']]
--- -1 for jumping backwards.
-vim.cmd[[
-inoremap <silent> <S-Tab> <cmd>lua require'luasnip'.jump(-1)<Cr>
-
-snoremap <silent> <Tab> <cmd>lua require('luasnip').jump(1)<Cr>
-snoremap <silent> <S-Tab> <cmd>lua require('luasnip').jump(-1)<Cr>
-]]
-
--- For changing choices in choiceNodes (not strictly necessary for a basic setup).
-vim.cmd[[
-imap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
-smap <silent><expr> <C-E> luasnip#choice_active() ? '<Plug>luasnip-next-choice' : '<C-E>'
-]]
